2e26b614e8ffad2d6b48f60ff2f785fcde2a01fd
Heitor Danilo <heitordanilo2003@gmail.com>
2023-06-08 16:30:10 -0300

    renaming test_number_assingment test

--- src/main_test.c
+++ src/main_test.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-#include <token/actions_test.h>
+#include <token/token_test.h>
 
 int main() {
-    test_variable_and_function_assignment_tokenization();
-    test_delimiters_tokenization();
-    test_single_symbol_tokenization();
-    test_compound_symbol_tokenization();
+    test_number_assignment();
+    test_bool_assignment();
+    test_delimiters();
+    test_single_symbol();
+    test_compound_symbol();
     test_semicolon_symbol_independence();
-    test_numbers_tokenization();
 
     printf("\n\x1b[32mAll tests pass.\x1b[0m\n");
 
