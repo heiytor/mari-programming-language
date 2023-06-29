4b60e347d30d0f728918b32583b6a58811c42575
Heitor Danilo <heitordanilo2003@gmail.com>
2023-06-28 22:47:37 -0300

    chore: rename methods name

--- src/parser/init.c
+++ src/parser/init.c
@@ -2,6 +2,7 @@
 
 #include <lexer/lib.h>
 #include <parser/lib.h>
+#include <token/lib.h>
 
 struct Parser *new_parser(struct Lexer *lexer) {
     struct Parser *parser = malloc(sizeof(struct Parser));
@@ -11,10 +12,21 @@ struct Parser *new_parser(struct Lexer *lexer) {
     }
 
     parser->lexer = lexer;
-    parser->next_token = __P_next_token;
 
-    parser->next_token(parser);
-    parser->next_token(parser);    
+    parser->consume_token = __PARSER_consume_token;
+    parser->next_token_is = __PARSER_next_token_is;
+    parser->curr_token_is = __PARSER_curr_token_is;
+    parser->consume_or_ignore_if_next_token_is = __PARSER_consume_or_ignore_if_next_token_is;
+    
+    parser->parse_statement = __PARSER_parse_statement;
+    parser->parse_program = __PARSER_parse_program;
+    parser->parse_var_statement = __PARSER_parse_var_statement;
+    parser->parse_return_statement = __PARSER_parse_return_statement;
+    parser->parse_expression_statement = __PARSER_parse_expression_statement;
+
+    // We need to advance two tokens to set both parser->curr_token and parser->next_token
+    parser->consume_token(parser);
+    parser->consume_token(parser);
 
     return parser;
 }
