d68e45d37d7575c32305643ed7e0f09a6965448d
Heitor Danilo <heitordanilo2003@gmail.com>
2023-06-29 10:31:45 -0300

    refactor: adding mappers functions

--- src/token/utils.c
+++ src/token/utils.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include <langdef.h>
 #include <token/lib.h>
 
 /**
@@ -15,4 +16,80 @@ void print_token(struct Token *tok) {
             tok->column);
 
     return;
+}
+
+struct TokenMap keywords[] = {
+    {"func", FUNCTION},
+    {"var", VARIABLE},
+    {"true", TRUE},
+    {"false", FALSE},
+    {"if", IF},
+    {"elif", ELSE_IF},
+    {"else", ELSE},
+    {"return", RETURN},
+    {"mut", MUTABLE},
+    {"as", AS},
+    {"returns", RETURNS},
+};
+
+uint8 get_ident_code(char* ident) {
+    struct TokenMap *kw = keywords;
+
+    while (kw->str) {
+        if (strcmp(ident, kw->str) == 0) {
+            return kw->token_code;
+        }
+
+        ++kw;
+    }
+
+    return IDENT;
+}
+
+struct TokenMap symbols[] = {
+    {"=", ASSIGN},
+    {"==", EQUAL},
+    {"+", PLUS},
+    {"+=", PLUS_ASSIGN},
+    {"++", INCREMENT},
+    {"-", MINUS},
+    {"--", DECREMENT},
+    {"-=", MINUS_ASSIGN},
+    {"*", ASTERISK},
+    {"**", POWER},
+    {"*=", ASTERISK_ASSIGN},
+    {"!=", NOT_EQUAL},
+    {"/", SLASH},
+    {"/=", SLASH_ASSIGN},
+    {"<", LESS_THAN},
+    {"<=", LESS_THAN_EQUAL},
+    {">", GREATER_THAN},
+    {">=", GREATER_THAN_EQUAL},
+    {"!", BANG},
+    {"!!", DOUBLE_BANG},
+    {"{", L_BRACE},
+    {"}", R_BRACE},
+    {"(", L_PAREN},
+    {")", R_PAREN},
+    {";", SEMICOLON},
+    {",", COMMA},
+    {".", DOT},
+    {"?", QUESTION},
+    {":", COLON},
+    {"%%", MODULO},
+    {"\0", END_OF_FILE},
+};
+
+uint8 get_symbol_code(char *symbol) {
+    struct TokenMap* sy = symbols;
+
+    while (sy->str) {
+        if (strcmp(symbol, sy->str) == 0) {
+            return sy->token_code;
+        }
+
+        ++sy;
+    }
+
+    return symbol ? ILLEGAL : END_OF_FILE;
 }
